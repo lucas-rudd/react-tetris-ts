297579afc50cb85859cd9ef69b8cad845adf7b34
Lucas Rudd <lucas.rudd@LRUDD-MAC.fourwindsinteractive.com>
2020-03-24 10:34:47 -0600

    adding title to base dom element"

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import Tetris from "./components/tetris";
+import title from "./assets/Title.png";
 import "./styles/styles.css";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <Tetris
-    boardWidth={14}
-    boardHeight={20}
-    holdingHeight={5}
-    holdingWidth={5}
-  />,
+  <div className="container">
+    <div className="image-container">
+      <img className="tetris-title" src={title} />
+    </div>
+    <Tetris
+      boardWidth={14}
+      boardHeight={20}
+      holdingHeight={5}
+      holdingWidth={5}
+    />
+  </div>,
   document.getElementById("root")
 );
 
