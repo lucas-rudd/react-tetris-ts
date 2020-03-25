7851d495a520d0a53673c145f851e617126cd435
Lucas Rudd <lucas.rudd@LRUDD-MAC.fourwindsinteractive.com>
2020-03-25 08:43:52 -0600

    added declarations for sound files

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="react-scripts" />
+declare module "*.mp3" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.wav" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.m4a" {
+  const src: string;
+  export default src;
+}
